007a4b6bd6ac199725c9c8cea54a21833e61bfb9
Anatoly Ponomarev <i@foundfun.ru>
2023-10-04 19:42:55 +0500

    Add Save

--- Assets/CodeBase/Infrastructure/Services/SaveLoad/SaveLoadService.cs
+++ Assets/CodeBase/Infrastructure/Services/SaveLoad/SaveLoadService.cs
@@ -1,4 +1,6 @@
 using CodeBase.Data;
+using CodeBase.Infrastructure.Factory;
+using CodeBase.Infrastructure.Services.PersistentProgress;
 using UnityEngine;
 
 namespace CodeBase.Infrastructure.Services.SaveLoad
@@ -6,12 +8,25 @@ namespace CodeBase.Infrastructure.Services.SaveLoad
     public class SaveLoadService : ISaveLoadService
     {
         private const string ProgressKey = "Progress";
+        
+        private readonly IPersistentProgressService _progressService;
+        private readonly IGameFactory _gameFactory;
+
+        public SaveLoadService(IPersistentProgressService progressService, IGameFactory gameFactory)
+        {
+            _progressService = progressService;
+            _gameFactory = gameFactory;
+        }
 
         public void SaveProgress()
         {
+            foreach (ISavedProgress progressesWriter in _gameFactory.ProgressesWriters)
+                progressesWriter.UpdateProgress(_progressService.Progress);
+            
+            PlayerPrefs.SetString(ProgressKey, _progressService.Progress.ToJson());
         }
 
-        public PlayerProgress LoadProgress() => 
+        public PlayerProgress LoadProgress() =>
             PlayerPrefs.GetString(ProgressKey)?.ToDeserialized<PlayerProgress>();
     }
 }
