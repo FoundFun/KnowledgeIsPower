74aee4d1b5810246d875ced1c8a62de92ce0998b
Anatoly Ponomarev <i@foundfun.ru>
2023-10-08 13:31:40 +0500

    Add MonsterStaticData

---
+++ Assets/CodeBase/StaticData/MonsterStaticData.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace CodeBase.StaticData
+{
+    [CreateAssetMenu(fileName = "MonsterData",menuName = "StaticData/Monster", order = 51)]
+    public class MonsterStaticData : ScriptableObject
+    {
+        public MonsterTypeId MonsterTypeId;
+        
+        [Range(1, 100)]
+        public int Health;
+        
+        [Range(1f, 30f)]
+        public float Damage;
+
+        [Range(0.5f, 1F)]
+        public float EffectiveDistance;
+        
+        [Range(0.5f, 1f)]
+        public float Cleavage;
+
+        public GameObject Prefab;
+    }
+}
